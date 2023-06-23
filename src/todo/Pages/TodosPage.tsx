c8b61dec97abcac3fde89b33e7f6064be0f2e9cd
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2023-06-22 18:34:11 -0600

    WIP: todo app page

--- src/todo/Pages/TodosPage.tsx
+++ src/todo/Pages/TodosPage.tsx
@@ -1,22 +1,30 @@
 import React, { useContext } from 'react'
 import { TodoContext } from '../context/'
 import { Todo, TodoDetail, TodoTitle } from '../components'
+import { SearchInput } from '../components'
+import { Button } from '../../auth/components'
 
 
 
 export const TodosPage = () => {
   const { todos } = useContext(TodoContext)
   return (
-    <div className="h-screen bg-slate-200 flex justify-center items-center animate__animated animate__fadeIn animate__faster">
-      <div className="bg-white border-black md:w-6/12 w-8/12 p-10 shadow-xl divide-y divide-slate-300 ">
+    <div className="h-screen bg-slate-200 flex justify-center flex-col gap-4 items-center animate__animated animate__fadeIn animate__faster">
+      <div className=" md:w-6/12 w-11/12">
+        <div className="flex">
+          <SearchInput className="w-11/12" />
+          <Button type="button">Search</Button>
+        </div>
+      <div className="bg-white shadow-xl divide-y divide-slate-300">
         {
           todos?.map((todo)=>(
-            <Todo key={todo.id} todo={todo}>
+            <Todo key={todo.id} todo={todo} className="p-8">
               <TodoTitle />
               <TodoDetail />
             </Todo>
           ))
         }
+      </div>
       </div>
     </div>
   )
